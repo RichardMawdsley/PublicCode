0f676e9666dc0f04a1689c1e502cf6b7caa5bbba
Richard Mawdsley <richmawdsley@gmail.com>
2022-03-03 13:09:42 +0000

    Update DisablePowerSleepStates.ps1

--- DisablePowerSleepStates.ps1
+++ DisablePowerSleepStates.ps1
@@ -4,45 +4,51 @@
     
     .EXAMPLE
     .\DisablePowerSleepStates.ps1
-
     .OUTPUTS
     N/A
-
     .NOTES
     Written by: Rich Mawdsley (Ensono)
     Date: 25/02/2022
 #>
 
-Write-Host "Disabing all sleep display/power states in current power profile $ActiveSchemeGUID"
+#Power GUIDS:  https://docs.microsoft.com/en-us/windows/win32/power/power-policy-settings
 
-try{
-$ActiveSchemeGUID = (powercfg.exe /getactivescheme).split(' ')[5,6]
-}
-Catch
-{
-Write-Host "Failed to retrieve current power profile name"
-}
-
-$Options = @(
-"monitor-timeout-ac"
-"monitor-timeout-dc"
-"disk-timeout-ac"
-"disk-timeout-dc"
-"standby-timeout-ac"
-"standby-timeout-dc"
-"hibernate-timeout-ac"
-"hibernate-timeout-dc"
+$Schemes = @(
+    "381b4222-f694-41f0-9685-ff5bb260df2e" # Balanced
+    "a1841308-3541-4fab-bc81-f71556f20b4a" # Power saver
+    "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c" # High performance - Note last in last so its the last one set
 )
 
-ForEach ($option in $Options){
-try{
-Write-Host "Setting timeout of $Option to 0"
-powercfg.exe /CHANGE $Option 0
-}
-Catch
-{
-Write-Host "Failed to change power option for $Option"
-}
+$Options = @(
+    "monitor-timeout-ac"
+    "monitor-timeout-dc"
+    "disk-timeout-ac"
+    "disk-timeout-dc"
+    "standby-timeout-ac"
+    "standby-timeout-dc"
+    "hibernate-timeout-ac"
+    "hibernate-timeout-dc"
+)
+
+Foreach ($Scheme in $schemes) {
+    try {
+        powercfg.exe /SetActive $Scheme
+        $ActiveSchemeGUID = (powercfg.exe /getactivescheme).split(' ')[5, 6]
+        Write-Host "Disabing all sleep display/power states in power profile $ActiveSchemeGUID"
+    }
+    Catch {
+        Write-Host "Failed to retrieve power profile name"
+    }
+
+    ForEach ($option in $Options) {
+        try {
+            Write-Host "Setting timeout of $Option to 0"
+            powercfg.exe /CHANGE $Option 0
+        }
+        Catch {
+            Write-Host "Failed to change power option for $Option"
+        }
+    }
 }
 
 Write-Host "Power change complete"
